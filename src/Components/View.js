d3cc2a677981930ce7715048d5d1ff88727e0098
M Kumaran <veerammalkumaran@gmail.com>
2021-10-26 20:16:07 +0530

    server data changed as array and flattern in UI

--- src/Components/View.js
+++ src/Components/View.js
@@ -2,25 +2,38 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { SERVER_URL } from "../Util/constant";
 import { PageHeader } from "antd";
+import { useState } from "react";
 
 const View = () => {
-  const { data: dataResponse } = useQuery("data", () => axios.get(`${SERVER_URL}/data`), { refetchInterval: 500 });
-  const fetchHmi = useQuery("hmi", () => axios.get(`${SERVER_URL}/hmi`), { enabled: !!dataResponse });
-  //const [data, setData] = useState({ tag1: 20, tag2: 5, tag3: 200, tagcolor1: "red", tagcolor2: "blue" });
+  const [plcData, setPlcData] = useState(null);
+  useQuery("data", () => axios.get(`${SERVER_URL}/data`), {
+    refetchInterval: 500,
+    onSuccess: (d) => {
+      //flattern the array
+      const results = d.data;
+      const flatternData = {};
+      results.forEach((res) => {
+        flatternData[res.name] = res.val;
+      });
+      setPlcData(flatternData);
+      console.log(flatternData);
+    },
+  });
+  const fetchHmi = useQuery("hmi", () => axios.get(`${SERVER_URL}/hmi`), { enabled: !!plcData });
 
   return (
     <PageHeader ghost={false} title="HMI Viewer" subTitle="view">
       <svg viewBox="0 0 800 1200" xmlns="http://www.w3.org/2000/svg">
-        {dataResponse &&
+        {plcData &&
           fetchHmi.data?.data?.objects?.map((comp, idx) => {
             if (comp.type === "circle") {
-              return <circle key={idx} cx={comp.left} cy={comp.top} r={dataResponse.data[comp.extra.radius]} fill="transparent" stroke="black" />;
+              return <circle key={idx} cx={comp.left} cy={comp.top} r={plcData[comp.extra.radius]} fill="transparent" stroke="black" />;
             } else if (comp.type === "rect") {
-              return <rect key={idx} x={comp.left} y={comp.top} width={dataResponse.data[comp.extra.width]} height={comp.height} fill="transparent" stroke="black" />;
+              return <rect key={idx} x={comp.left} y={comp.top} width={plcData[comp.extra.width]} height={comp.height} fill="transparent" stroke="black" />;
             } else if (comp.type === "text") {
               return (
                 <text key={idx} x={comp.left} y={comp.top} fill="black">
-                  {dataResponse.data[comp.extra.text]}
+                  {plcData[comp.extra.text]}
                 </text>
               );
             }
